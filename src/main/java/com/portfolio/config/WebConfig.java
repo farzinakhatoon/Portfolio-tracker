4c27c73a8a89b899cc9dc4afa539e912c28670cf
farzinakhatoon <136377484+farzinakhatoon@users.noreply.github.com>
2024-12-29 18:02:50 +0530

    Update WebConfig.java

--- src/main/java/com/portfolio/config/WebConfig.java
+++ src/main/java/com/portfolio/config/WebConfig.java
@@ -1,7 +1,5 @@
 package com.portfolio.config;
 
-package com.portfolio.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
