66eb53cd33ca07ec5eab8f0aa3b2e662987e3b06
farzinakhatoon <136377484+farzinakhatoon@users.noreply.github.com>
2024-12-29 17:59:19 +0530

    Add files via upload

---
+++ src/main/java/com/portfolio/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.portfolio.config;
+
+package com.portfolio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Replace with the actual frontend URL (e.g., CodeSandbox or other
+        // environments)
+        registry.addMapping("/**").allowedOrigins("https://rpd2nf.csb.app/");
+    }
+}
