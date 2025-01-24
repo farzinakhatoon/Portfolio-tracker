1508a90c020408e50171699f2deea6a3355779ed
farzinakhatoon <136377484+farzinakhatoon@users.noreply.github.com>
2025-01-24 20:22:21 +0530

    Update PortfolioTrackerApplication.java

--- src/main/java/com/portfolio/PortfolioTrackerApplication.java
+++ src/main/java/com/portfolio/PortfolioTrackerApplication.java
@@ -2,12 +2,16 @@ package com.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-
+@CrossOrigin(origins = "https://rpd2nf.csb.app/") 
+    
 public class PortfolioTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioTrackerApplication.class, args);
     }
 }
+
+
