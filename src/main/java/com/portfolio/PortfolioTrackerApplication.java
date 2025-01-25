285742503cbb8fd038847e7b71bea925ef4080a9
farzinakhatoon <136377484+farzinakhatoon@users.noreply.github.com>
2025-01-25 18:51:25 +0530

    Update PortfolioTrackerApplication.java

--- src/main/java/com/portfolio/PortfolioTrackerApplication.java
+++ src/main/java/com/portfolio/PortfolioTrackerApplication.java
@@ -4,30 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@SpringBootApplication    
+@SpringBootApplication
+@CrossOrigin(origins = "https://dxy42g.csb.app/") 
+    
 public class PortfolioTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioTrackerApplication.class, args);
     }
 }
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://dxy42g.csb.app/") // Add more origins if needed
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
-
-
