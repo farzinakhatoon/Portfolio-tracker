1ede8b4dba1f0732e80a623a4576fc8dfe62e4e8
farzinakhatoon <136377484+farzinakhatoon@users.noreply.github.com>
2025-01-26 11:12:06 +0530

    Update PortfolioTrackerApplication.java

--- src/main/java/com/portfolio/PortfolioTrackerApplication.java
+++ src/main/java/com/portfolio/PortfolioTrackerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "https://23xdcq.csb.app/") 
+@CrossOrigin(origins = "https://23xdcq.csb.app/","famous-bunny-8b432f.netlify.app","*") 
     
 public class PortfolioTrackerApplication {
 
